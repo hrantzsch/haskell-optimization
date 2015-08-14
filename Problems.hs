c374e6f9b94c2ae0b87078f28ae6906c68c16068
Hannes Rantzsch <hannes.rantzsch@student.hpi.uni-potsdam.de>
2015-08-14 14:52:37 +0200

    royalRoads

--- Problems.hs
+++ Problems.hs
@@ -1,6 +1,7 @@
 module Problems where
 
 import           BinaryString
+import qualified Data.Foldable as F
 import           Data.Maybe    (fromMaybe)
 import qualified Data.Sequence as S
 
@@ -15,3 +16,11 @@ binVal :: BinaryString -> Int
 binVal bs = sum $ fmap f b
   where f (x,y) = y * 2 ^ x
         b = S.zip (S.fromList [0..S.length bs]) $ S.reverse bs
+
+royalRoads :: BinaryString -> Int -> Int
+royalRoads bs k
+  | S.null bs = 0
+  | otherwise = allOnes firstK + royalRoads remainder k
+                  where (firstK, remainder) = S.splitAt k bs
+                        allOnes bs | all (==1) $ F.toList bs = 1
+                                   | otherwise               = 0
